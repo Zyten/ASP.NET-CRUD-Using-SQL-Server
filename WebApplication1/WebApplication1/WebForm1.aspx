4364d03fdff7e087561849dce236a8b16b1beed3
zyten <hackeduser@hackermail.com>
2016-05-02 15:18:42 +0800

    initial commit

---
+++ WebApplication1/WebApplication1/WebForm1.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+<!--#include file="header.inc"-->
+	<div class="clearfix"></div>
+
+<div class="clearfix"></div><br />
+    <div class="container">
+    <form id="form1" runat="server">
+        <div class="GridviewDiv">
+            <asp:GridView runat="server" ID="gvDetails" ShowFooter="true" AllowPaging="true" PageSize="10" AutoGenerateColumns="false" DataKeyNames="book_id,book_name,book_author,book_ISBN" OnPageIndexChanging="gvDetails_PageIndexChanging" OnRowCancelingEdit="gvDetails_RowCancelingEdit"
+                OnRowEditing="gvDetails_RowEditing" OnRowUpdating="gvDetails_RowUpdating" OnRowDeleting="gvDetails_RowDeleting" OnRowCommand ="gvDetails_RowCommand" CssClass="table table-bordered table-responsive">
+                <HeaderStyle CssClass="headerstyle" />
+                    <Columns>
+
+                        <asp:BoundField DataField="book_id" HeaderText="Book Id" ReadOnly="true" />
+
+                        <asp:TemplateField HeaderText="Book Name">
+                            <ItemTemplate>
+                                <asp:Label ID="lblBookName" runat="server" Text='<%# Eval("book_name")%>'/>
+                            </ItemTemplate>
+
+                            <EditItemTemplate>
+                                <asp:TextBox ID="txtBookName" runat="server" Text='<%# Eval("book_name")%>'/>
+                            </EditItemTemplate>
+
+                            <FooterTemplate>
+                                    <asp:TextBox ID="txtbname" runat="server" />
+                            </FooterTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText = "Book Author">
+                            <ItemTemplate>
+                                <asp:Label ID="lblBookAuthor" runat="server" Text='<%# Eval("book_author")%>'></asp:Label>
+                            </ItemTemplate>
+
+                            <EditItemTemplate>
+                                <asp:TextBox ID="txtBookAuthor" runat="server" Text='<%# Eval("book_author")%>'/>
+                            </EditItemTemplate>
+
+                            <FooterTemplate>
+                                <asp:TextBox ID="txtbauthor" runat="server" />
+                            </FooterTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText = "Book ISBN">
+                            <ItemTemplate>
+                                <asp:Label ID="lblBookISBN" runat="server" Text='<%# Eval("book_ISBN")%>'></asp:Label>
+                            </ItemTemplate>
+
+                            <EditItemTemplate>
+                                <asp:TextBox ID="txtBookISBN" runat="server" Text='<%# Eval("book_ISBN")%>'/>
+                            </EditItemTemplate>
+
+                            <FooterTemplate>
+                                <asp:TextBox ID="txtbISBN" runat="server" />
+                                <asp:LinkButton ID="btnAdd" CommandName="AddNew" runat="server" CssClass="btn btn-large btn-info pull-right"><i class="glyphicon glyphicon-plus"></i> &nbsp; Add Records</asp:LinkButton>   
+                            </FooterTemplate>
+                        </asp:TemplateField>
+						<asp:CommandField ShowEditButton="True" ShowDeleteButton="true" />
+                    </Columns>
+
+            </asp:GridView>
+            <div id="success_alert" class="alert alert-success" runat="server" Visible="false">
+    	        <asp:Label ID="lblresult" runat="server"></asp:Label>
+		    </div>
+        </div>
+        <script type="text/javascript">
+            function HideLabel() {
+                var seconds = 5;
+                setTimeout(function () {
+                    document.getElementById("<%=success_alert.ClientID %>").style.display = "none";
+                }, seconds * 1000);
+            };
+    </script>
+    </form>
+    </div>
+    <!--#include file="footer.inc"-->
---
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridview();
+            }
+        }
+        protected void BindGridview()
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection("Data Source=ZYTEN-PC\\SQLEXPRESS;Integrated Security=true;Initial Catalog=book_details"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("crudoperations", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@status", "SELECT");
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    gvDetails.DataSource = ds;
+                    gvDetails.DataBind();
+                }
+                else
+                {
+                    ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                    gvDetails.DataSource = ds;
+                    gvDetails.DataBind();
+                    int columncount = gvDetails.Rows[0].Cells.Count;
+                    gvDetails.Rows[0].Cells.Clear();
+                    gvDetails.Rows[0].Cells.Add(new TableCell());
+                    gvDetails.Rows[0].Cells[0].ColumnSpan = columncount;
+                    gvDetails.Rows[0].Cells[0].Text = "No Records Found";
+                }
+            }
+        }
+
+        protected void gvDetails_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.Equals("AddNew"))
+            {
+                TextBox txtbname = (TextBox)gvDetails.FooterRow.FindControl("txtbname");
+                TextBox txtbauthor = (TextBox)gvDetails.FooterRow.FindControl("txtbauthor");
+                TextBox txtbISBN = (TextBox)gvDetails.FooterRow.FindControl("txtbISBN");
+                crudoperations("INSERT", txtbname.Text, txtbauthor.Text, txtbISBN.Text, 0);
+            }
+        }
+
+        protected void gvDetails_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvDetails.EditIndex = e.NewEditIndex;
+            BindGridview();
+        }
+
+        protected void gvDetails_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvDetails.EditIndex = -1;
+            BindGridview();
+        }
+
+        protected void gvDetails_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvDetails.PageIndex = e.NewPageIndex;
+            BindGridview();
+        }
+
+        protected void gvDetails_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int book_id = Convert.ToInt32(gvDetails.DataKeys[e.RowIndex].Values["book_id"].ToString());
+            TextBox txtbookname = (TextBox)gvDetails.Rows[e.RowIndex].FindControl("txtBookName");
+            TextBox txtbookauthor = (TextBox)gvDetails.Rows[e.RowIndex].FindControl("txtBookAuthor");
+            TextBox txtbISBN = (TextBox)gvDetails.FooterRow.FindControl("txtbISBN");
+            crudoperations("UPDATE", txtbookname.Text, txtbookauthor.Text, txtbISBN.Text, book_id);
+        }
+
+        protected void gvDetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int book_id = Convert.ToInt32(gvDetails.DataKeys[e.RowIndex].Values["book_id"].ToString());
+            string book_name = gvDetails.DataKeys[e.RowIndex].Values["book_name"].ToString();
+            crudoperations("DELETE", book_name, "", "", book_id);
+        }
+
+        protected void crudoperations(string status, string book_name, string book_author, string book_ISBN, int book_id)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=ZYTEN-PC\\SQLEXPRESS;Integrated Security=true;Initial Catalog=book_details"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("crudoperations", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (status == "INSERT")
+                {
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@book_name", book_name);
+                    cmd.Parameters.AddWithValue("@book_author", book_author);
+                    cmd.Parameters.AddWithValue("@book_ISBN", book_ISBN);
+                }
+                else if (status == "UPDATE")
+                {
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@book_name", book_name);
+                    cmd.Parameters.AddWithValue("@book_author", book_author);
+                    cmd.Parameters.AddWithValue("@book_ISBN", book_ISBN);
+                    cmd.Parameters.AddWithValue("@book_id", book_id);
+                }
+                else if (status == "DELETE")
+                {
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@book_id", book_id);
+                }
+                cmd.ExecuteNonQuery();
+                lblresult.ForeColor = Color.Green;
+                string append = "";
+                if (status.ToLower() == "update" || status.ToLower() == "delete")
+                    append = "d.";
+                else
+                    append = "ed.";
+                lblresult.Text = "<strong>Success!</strong> " + " Book " + book_name + " " + status.ToLower() + append;
+                success_alert.Visible = true;
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "HideLabel();", true);
+                gvDetails.EditIndex = -1;
+                BindGridview();
+            }
+        }
+    }
+}
